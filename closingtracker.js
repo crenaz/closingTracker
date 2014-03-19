a9e5a9bbe0de872a7df76000aa4a6e6a116bff37
crenaz <crenaz@gmail.com>
2014-03-19 17:22:50 -0400

    first commit, exciting!

---
+++ closingtracker.js
@@ -0,0 +1,8 @@
+$( document ).ready(function() {
+
+    $( "a[id$='exhibitions/']" ).click(function( event ) {
+        event.preventDefault();
+        window.location.href = "http://robertoparadise.com/exhibitions/watermelon-smiley-faces-ice-cream-popsicles-avocado-and-sun-katherine-bernhardt-2/";
+    });
+
+});
